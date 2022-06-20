887d2bc8d022805833cbfbc6aad793ba762bc5fe
Timofey Krivenkov <drake9100@mail.ru>
2022-06-20 20:33:10 +0300

    UPD: UI компоненты кнопок и тултип

--- src/js/_vars.js
+++ src/js/_vars.js
@@ -3,4 +3,6 @@ export default {
   documentEl: document,
   htmlEl: document.documentElement,
   bodyEl: document.body,
+  heroTooltipText: document.querySelector('.hero__tooltip .tooltip__txt'),
+  heroTooltipBtn: document.querySelector('.hero__tooltip .tooltip__btn'),
 }
--- src/js/components/utils.js
+++ src/js/components/utils.js
@@ -1,4 +1,11 @@
 import { disableScroll } from '../functions/disable-scroll';
 import { enableScroll } from '../functions/enable-scroll';
 import { getHeaderHeight } from '../functions/header-height';
+import { createPopper, right} from '@popperjs/core';
+import _vars from "../_vars.js"
+
 getHeaderHeight();
+
+createPopper(_vars.heroTooltipBtn, _vars.heroTooltipText, {
+  placement: 'top-end'
+});
